f6de8a2db5a85a07d3266e0ae1c2d47fb90729ed
anmariyafrancis <60163786+anmariyafrancis@users.noreply.github.com>
2021-03-20 23:52:57 -0400

    created DynamicPage class

--- pages/generic.js
+++ pages/generic.js
@@ -8,3 +8,13 @@ import Page from 'components/views/page'
 import Header from 'components/views/partials/header'
 import Footer from 'components/views/partials/footer'
 import Request from 'utils/request';
+
+class DynamicPage extends React.Component {
+
+  static async getInitialProps({ req, query }) {
+    const Response = await Request.getGlobals();
+    const pageResponse = await Request.getObject(query.pagename);
+    const page = pageResponse.object;
+    const globals = mapGlobals(Response.objects);
+    return { globals, page };
+  }
