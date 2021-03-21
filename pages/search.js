779477b4c42b9f29a0f47f622041e82959c1e8f4
anmariyafrancis <60163786+anmariyafrancis@users.noreply.github.com>
2021-03-20 22:30:08 -0400

    created class searchpage

--- pages/search.js
+++ pages/search.js
@@ -6,3 +6,14 @@ import Search from "components/views/search";
 import Header from "components/views/partials/header";
 import Footer from "components/views/partials/footer";
 import Request from "utils/request";
+
+class SearchPage extends React.Component {
+  static async getInitialProps({ req, query }) {
+    const Response = await Request.getGlobals();
+    const bucketResponse = await Request.getObjects();
+    const searchResponse = await Request.getObject('search');
+    const bucket = bucketResponse.objects;
+    const search = searchResponse.object;
+    const globals = mapGlobals(Response.objects);
+    return { globals, bucket, search };
+  }
