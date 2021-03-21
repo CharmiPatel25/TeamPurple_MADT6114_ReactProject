954a3ee9747e5af92067790955fb0c459a98c7be
anmariyafrancis <60163786+anmariyafrancis@users.noreply.github.com>
2021-03-20 22:10:13 -0400

    imported react files

---
+++ pages/search.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { mapGlobals } from "utils/helperFuncs";
+import Head from "next/head";
+import Meta from "components/widgets/Meta";
+import Search from "components/views/search";
+import Header from "components/views/partials/header";
+import Footer from "components/views/partials/footer";
+import Request from "utils/request";
