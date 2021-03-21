a5d2a97a178c358be45fcbd541a7469f3e544a54
anmariyafrancis <60163786+anmariyafrancis@users.noreply.github.com>
2021-03-20 23:49:07 -0400

    import react files in generic.js

---
+++ pages/generic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Router from 'next/router';
+
+import { mapGlobals } from 'utils/helperFuncs';
+import Head from 'next/head';
+import Meta from 'components/widgets/Meta';
+import Page from 'components/views/page'
+import Header from 'components/views/partials/header'
+import Footer from 'components/views/partials/footer'
+import Request from 'utils/request';
